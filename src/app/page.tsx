83c4b6470399aec2933b6c879cd4cdf53906f840
weihang-z <weihangzeng79@gmail.com>
2025-08-16 21:11:19 -0400

    fix loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { UserBotton } from "@/features/auth/components/user-botton";
 import { useCreateWorkspaceModal } from "@/features/workspace/store/use-create-workspace-modal";
 import { useGetWorkspaces } from "@/features/workspace/api/use-get-workspaces";
 import { useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
 import { Modals } from "@/components/Modals";
+import { Loader } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
@@ -28,7 +28,9 @@ export default function Home() {
   return (
     <div>
       <Modals />
-      <UserBotton />
+      <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
     </div>
   );
 }
